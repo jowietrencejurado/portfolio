854cf9adfbcf6382f1c6ff3cd26a4c53158e22c2
jowie <jowie.jurado@simplevia.com>
2024-03-28 22:08:42 +0800

    fix pictures

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -94,7 +94,7 @@
 									<v-col v-for="item in tools" cols="2">
 										<v-card class="mx-auto py-2" max-height="150" :href="item.link" target="_blank" rel="noopener"
 											variant="tonal" elevation="2">
-											<v-img :src="getImage(item.logo, 'logo')" height="70" class="mx-auto my-3"></v-img>
+											<v-img :src="item.logo" height="70" class="mx-auto my-3"></v-img>
 											<v-card-title class="text-center skill-title">{{ item.name }}</v-card-title>
 										</v-card>
 									</v-col>
@@ -139,7 +139,7 @@
 															hide-delimiters>
 															<template v-for="subItem in revlvProjects">
 																<v-carousel-item
-																	:src="getImage(subItem.image, 'projects')">
+																	:src="subItem.image">
 																	<div class="d-flex fill-height justify-start align-end">
 																		<div class="text-h6">
 																			{{ subItem.caption }}
@@ -154,7 +154,7 @@
 															hide-delimiters>
 															<template v-for="subItem in simpleviaProjects">
 																<v-carousel-item
-																	:src="getImage(subItem.image, 'projects')">
+																	:src="subItem.image">
 																	<div class="d-flex fill-height justify-start align-end">
 																		<div class="text-h6">
 																			{{ subItem.caption }}
@@ -184,24 +184,99 @@ import { useTheme } from 'vuetify';
 import { reactive } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { onBeforeMount } from 'vue';
-import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
-// import airtableLogo from '@/assets/logo/airtable.png';
+// Skills
+import laravel from '@/assets/logo/laravel.png';
+import php from '@/assets/logo/php.png';
+import vuejs from '@/assets/logo/vuejs.png';
+import nuxtjs from '@/assets/logo/nuxtjs.png';
+import html from '@/assets/logo/html.png';
+import css3 from '@/assets/logo/css3.png';
+import cSharp from '@/assets/logo/c-sharp.png';
+import aspnet from '@/assets/logo/aspnet.png';
+import react from '@/assets/logo/react.png';
+import javascript from '@/assets/logo/javascript.png';
+import jquery from '@/assets/logo/jquery.png';
+import vuetifyjs from '@/assets/logo/vuetifyjs.svg';
+import tailwind from '@/assets/logo/tailwind.png';
+import bootstrap from '@/assets/logo/bootstrap.png';
+import uikit from '@/assets/logo/uikit.svg';
+import headlessui from '@/assets/logo/headlessui.png';
+import mysql from '@/assets/logo/mysql.png';
+import mariadb from '@/assets/logo/mariadb.png';
+import postgresql from '@/assets/logo/postgresql.png';
+import microsoftSqlServer from '@/assets/logo/microsoft-sql-server.png';
+import composer from '@/assets/logo/composer.png';
+import git from '@/assets/logo/git.png';
+import github from '@/assets/logo/github.png';
+import gitlab from '@/assets/logo/gitlab.svg';
+// Skills
+
+// Tools
+import nodejs from '@/assets/logo/nodejs.png';
+import nvm from '@/assets/logo/nvm.png';
+import vscode from '@/assets/logo/vscode.png';
+import visualStudio from '@/assets/logo/visual-studio.png';
+import sublimeText from '@/assets/logo/sublime-text.png';
+import datagrip from '@/assets/logo/datagrip.png';
+import ssms from '@/assets/logo/ssms.png';
+import mysqlWorkbench from '@/assets/logo/mysql-workbench.png';
+import xampp from '@/assets/logo/xampp.png';
+import wamp from '@/assets/logo/wamp.png';
+import vite from '@/assets/logo/vite.png';
+import postman from '@/assets/logo/postman.svg';
+import apache from '@/assets/logo/apache.png';
+import clickup from '@/assets/logo/clickup.png';
+import trello from '@/assets/logo/trello.png';
+import airtable from '@/assets/logo/airtable.png';
+import windows from '@/assets/logo/windows.png';
+import ubuntu from '@/assets/logo/ubuntu.png';
+import aws from '@/assets/logo/aws.png';
+import iis from '@/assets/logo/iis.png';
+import spatie from '@/assets/logo/spatie.png';
+import npm from '@/assets/logo/npm.png';
+// Tools
+
+// SimpleVia Projects
+import ais1 from '@/assets/projects/ais-1.png';
+import ais2 from '@/assets/projects/ais-2.png';
+import ais3 from '@/assets/projects/ais-3.png';
+import ais4 from '@/assets/projects/ais-4.png';
+import ais5 from '@/assets/projects/ais-5.png';
+import esign1 from '@/assets/projects/esign-1.png';
+import esign2 from '@/assets/projects/esign-2.png';
+import esign3 from '@/assets/projects/esign-3.png';
+import esign4 from '@/assets/projects/esign-4.png';
+import esign5 from '@/assets/projects/esign-5.png';
+import esign6 from '@/assets/projects/esign-6.png';
+import lmiis1 from '@/assets/projects/lmiis-6.png';
+import lmiis2 from '@/assets/projects/lmiis-6.png';
+import lmiis3 from '@/assets/projects/lmiis-6.png';
+import lmiis4 from '@/assets/projects/lmiis-6.png';
+import lmiis5 from '@/assets/projects/lmiis-6.png';
+import lmiis6 from '@/assets/projects/lmiis-6.png';
+// SimpleVia Projects
+
+// Revlv Projects
+import archiving1 from '@/assets/projects/archiving-home.png';
+import archiving2 from '@/assets/projects/archiving1.png';
+import archiving3 from '@/assets/projects/archiving2.png';
+import archiving4 from '@/assets/projects/archiving3.png';
+import archiving5 from '@/assets/projects/archiving4.png';
+import archiving6 from '@/assets/projects/archiving5.png';
+import archiving7 from '@/assets/projects/archiving6.png';
+import avlci1 from '@/assets/projects/avlci-1.png';
+import avlci2 from '@/assets/projects/avlci-2.png';
+import avlci3 from '@/assets/projects/avlci-3.png';
+import avlci4 from '@/assets/projects/avlci-4.png';
+import fis1 from '@/assets/projects/fis1.png';
+import fis2 from '@/assets/projects/fis2.png';
+import fis3 from '@/assets/projects/fis3.png';
+import fis4 from '@/assets/projects/fis4.png';
+import ristoken1 from '@/assets/projects/ristoken1.png';
+import ristoken2 from '@/assets/projects/ristoken2.png';
+import ristoken3 from '@/assets/projects/ristoken3.png';
+import ristoken4 from '@/assets/projects/ristoken4.png';
+// Revlv Projects
 
 const theme = useTheme();
 const router = useRouter();
@@ -231,99 +306,99 @@ const model = reactive({
 const skills = reactive([
 	{
 		link: 'https://laravel.com/',
-		logo: airtableLogo,
+		logo: laravel,
 		name: 'Laravel'
 	}, {
 		link: 'https://www.php.net/',
-		logo: 'php.png',
+		logo: php,
 		name: 'PHP'
 	}, {
 		link: 'https://vuejs.org/',
-		logo: 'vuejs.png',
+		logo: vuejs,
 		name: 'VueJS'
 	}, {
 		link: 'https://nuxt.com/',
-		logo: 'nuxtjs.png',
+		logo: nuxtjs,
 		name: 'NuxtJS'
 	}, {
 		link: 'https://www.tutorialspoint.com/html5/index.htm',
-		logo: 'html.png',
+		logo: html,
 		name: 'HTML5'
 	}, {
 		link: 'https://www.tutorialspoint.com/css/css3_tutorial.htm',
-		logo: 'css3.png',
+		logo: css3,
 		name: 'CSS3'
 	}, {
 		link: 'https://dotnet.microsoft.com/en-us/learntocode',
-		logo: 'c-sharp.png',
+		logo: cSharp,
 		name: 'C#'
 	}, {
 		link: 'https://dotnet.microsoft.com/en-us/apps/aspnet',
-		logo: 'aspnet.png',
+		logo: aspnet,
 		name: 'ASP.Net'
 	}, {
 		link: 'https://react.dev/',
-		logo: 'react.png',
+		logo: react,
 		name: 'ReactJS'
 	}, {
 		link: 'https://www.w3schools.com/js/',
-		logo: 'javascript.png',
+		logo: javascript,
 		name: 'JavaScript'
 	}, {
 		link: 'https://jquery.com/',
-		logo: 'jquery.png',
+		logo: jquery,
 		name: 'jQuery'
 	}, {
 		link: 'https://vuetifyjs.com/en/',
-		logo: 'vuetifyjs.svg',
+		logo: vuetifyjs,
 		name: 'VuetifyJS'
 	}, {
 		link: 'https://tailwindcss.com/',
-		logo: 'tailwind.png',
+		logo: tailwind,
 		name: 'Tailwind CSS'
 	}, {
 		link: 'https://getbootstrap.com/',
-		logo: 'bootstrap.png',
+		logo: bootstrap,
 		name: 'Bootstrap'
 	}, {
 		link: 'https://getuikit.com/',
-		logo: 'uikit.svg',
+		logo: uikit,
 		name: 'UIKit'
 	}, {
 		link: 'https://headlessui.com/',
-		logo: 'headlessui.png',
+		logo: headlessui,
 		name: 'Headless UI'
 	}, {
 		link: 'https://www.mysql.com/',
-		logo: 'mysql.png',
+		logo: mysql,
 		name: 'MySQL'
 	}, {
 		link: 'https://mariadb.org/',
-		logo: 'mariadb.png',
+		logo: mariadb,
 		name: 'MariaDB'
 	}, {
 		link: 'https://www.postgresql.org/',
-		logo: 'postgresql.png',
+		logo: postgresql,
 		name: 'PostgreSQL'
 	}, {
 		link: 'https://learn.microsoft.com/en-us/sql/sql-server/tutorials-for-sql-server-2016?view=sql-server-ver16/',
-		logo: 'microsoft-sql-server.png',
+		logo: microsoftSqlServer,
 		name: 'Microsoft SQL'
 	}, {
 		link: 'https://getcomposer.org/',
-		logo: 'composer.png',
+		logo: composer,
 		name: 'Composer'
 	}, {
 		link: 'https://git-scm.com/',
-		logo: 'git.png',
+		logo: git,
 		name: 'Git'
 	}, {
 		link: 'https://github.com/',
-		logo: 'github.png',
+		logo: github,
 		name: 'GitHub'
 	}, {
 		link: 'https://about.gitlab.com/',
-		logo: 'gitlab.svg',
+		logo: gitlab,
 		name: 'GitLab'
 	}
 ]);
@@ -331,91 +406,91 @@ const skills = reactive([
 const tools = reactive([
 	{
 		link: 'https://nodejs.org/en/about',
-		logo: 'nodejs.png',
+		logo: nodejs,
 		name: 'Node.js'
 	}, {
 		link: 'https://github.com/nvm-sh/nvm',
-		logo: 'nvm.png',
+		logo: nvm,
 		name: 'NVM'
 	}, {
 		link: 'https://code.visualstudio.com/',
-		logo: 'vscode.png',
+		logo: vscode,
 		name: 'VSCode'
 	}, {
 		link: 'https://visualstudio.microsoft.com/',
-		logo: 'visual-studio.png',
+		logo: visualStudio,
 		name: 'Visual Studio'
 	}, {
 		link: 'https://www.sublimetext.com/',
-		logo: 'sublime-text.png',
+		logo: sublimeText,
 		name: 'Sublime Text'
 	}, {
 		link: 'https://www.jetbrains.com/datagrip/',
-		logo: 'datagrip.png',
+		logo: datagrip,
 		name: 'DataGrip'
 	}, {
 		link: 'https://learn.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver16',
-		logo: 'ssms.png',
+		logo: ssms,
 		name: 'SSMS'
 	}, {
 		link: 'https://www.mysql.com/products/workbench/',
-		logo: 'mysql-workbench.png',
+		logo: mysqlWorkbench,
 		name: 'MySQL WorkBench'
 	}, {
 		link: 'https://www.apachefriends.org/',
-		logo: 'xampp.png',
+		logo:  xampp,
 		name: 'XAMPP'
 	}, {
 		link: 'https://www.wampserver.com/en/',
-		logo: 'wamp.png',
+		logo: wamp,
 		name: 'WAMP'
 	}, {
 		link: 'https://vitejs.dev/',
-		logo: 'vite.png',
+		logo: vite,
 		name: 'ViteJS'
 	}, {
 		link: 'https://www.postman.com/',
-		logo: 'postman.svg',
+		logo: postman,
 		name: 'Postman'
 	}, {
 		link: 'https://httpd.apache.org/',
-		logo: 'apache.png',
+		logo: apache,
 		name: 'Apache'
 	}, {
 		link: 'https://clickup.com/',
-		logo: 'clickup.png',
+		logo: clickup,
 		name: 'ClickUp'
 	}, {
 		link: 'https://trello.com/',
-		logo: 'trello.png',
+		logo: trello,
 		name: 'Trello'
 	}, {
 		link: 'https://www.airtable.com/',
-		logo: 'airtable.png',
+		logo: airtable,
 		name: 'AirTable'
 	}, {
 		link: 'https://www.microsoft.com/en-ph/windows',
-		logo: 'windows.png',
+		logo: windows,
 		name: 'Windows'
 	}, {
 		link: 'https://ubuntu.com/',
-		logo: 'ubuntu.png',
+		logo: ubuntu,
 		name: 'Ubuntu'
 	}, {
 		link: 'https://aws.amazon.com/',
-		logo: 'aws.png',
+		logo: aws,
 		name: 'AWS EC2'
 	}, {
 		link: 'https://www.iis.net/',
-		logo: 'iis.png',
+		logo: iis,
 		name: 'IIS'
 	}, {
 		link: 'https://spatie.be/',
-		logo: 'spatie.png',
+		logo: spatie,
 		name: 'Spatie'
 	}, {
 		link: 'https://www.npmjs.com/',
-		logo: 'npm.png',
+		logo: npm,
 		name: 'NPM'
 	},
 ]);
@@ -423,116 +498,116 @@ const tools = reactive([
 const simpleviaProjects = reactive([
 	{
 		caption: 'Makati AIS (Accounting Information System)',
-		image: 'ais-1.png'
+		image: ais1
 	}, {
 		caption: 'Makati AIS (Accounting Information System)',
-		image: 'ais-2.png'
+		image: ais2
 	}, {
 		caption: 'Makati AIS (Accounting Information System)',
-		image: 'ais-3.png'
+		image: ais3
 	}, {
 		caption: 'Makati AIS (Accounting Information System)',
-		image: 'ais-4.png'
+		image: ais4
 	}, {
 		caption: 'Makati AIS (Accounting Information System)',
-		image: 'ais-5.png'
+		image: ais5
 	}, {
 		caption: 'Makati E-Sign',
-		image: 'esign-1.png'
+		image: esign1
 	}, {
 		caption: 'Makati E-Sign',
-		image: 'esign-2.png'
+		image: esign2
 	}, {
 		caption: 'Makati E-Sign',
-		image: 'esign-3.png'
+		image: esign3
 	}, {
 		caption: 'Makati E-Sign',
-		image: 'esign-4.png'
+		image: esign4
 	}, {
 		caption: 'Makati E-Sign',
-		image: 'esign-5.png'
+		image: esign5
 	}, {
 		caption: 'Makati E-Sign',
-		image: 'esign-6.png'
+		image: esign6
 	}, {
 		caption: 'LMIIS (Logistic Management Inventory and Information System)',
-		image: 'lmiis-1.png'
+		image: lmiis1
 	}, {
 		caption: 'LMIIS (Logistic Management Inventory and Information System)',
-		image: 'lmiis-2.png'
+		image: lmiis2
 	}, {
 		caption: 'LMIIS (Logistic Management Inventory and Information System)',
-		image: 'lmiis-3.png'
+		image: lmiis3
 	}, {
 		caption: 'LMIIS (Logistic Management Inventory and Information System)',
-		image: 'lmiis-4.png'
+		image: lmiis4
 	}, {
 		caption: 'LMIIS (Logistic Management Inventory and Information System)',
-		image: 'lmiis-5.png'
+		image: lmiis5
 	}, {
 		caption: 'LMIIS (Logistic Management Inventory and Information System)',
-		image: 'lmiis-6.png'
+		image: lmiis6
 	}
 ]);
 
 const revlvProjects = reactive([
 	{
 		caption: 'Document Archiving',
-		image: 'archiving-home.png'
+		image:archiving1
 	}, {
 		caption: 'Document Archiving',
-		image: 'archiving1.png'
+		image: archiving2
 	}, {
 		caption: 'Document Archiving',
-		image: 'archiving2.png'
+		image: archiving3
 	}, {
 		caption: 'Document Archiving',
-		image: 'archiving3.png'
+		image: archiving4
 	}, {
 		caption: 'Document Archiving',
-		image: 'archiving4.png'
+		image: archiving5
 	}, {
 		caption: 'Document Archiving',
-		image: 'archiving5.png'
+		image: archiving6
 	}, {
 		caption: 'Document Archiving',
-		image: 'archiving6.png'
+		image: archiving7
 	}, {
 		caption: 'AVLCI (Astoria Vacation & Leisure Club Inc. System)',
-		image: 'avlci-1.png'
+		image: avlci1
 	}, {
 		caption: 'AVLCI (Astoria Vacation & Leisure Club Inc. System)',
-		image: 'avlci-2.png'
+		image: avlci2
 	}, {
 		caption: 'AVLCI (Astoria Vacation & Leisure Club Inc. System)',
-		image: 'avlci-3.png'
+		image: avlci3
 	}, {
 		caption: 'AVLCI (Astoria Vacation & Leisure Club Inc. System)',
-		image: 'avlci-4.png'
+		image: avlci4
 	}, {
 		caption: 'Farmer Information System',
-		image: 'fis1.png'
+		image: fis1
 	}, {
 		caption: 'Farmer Information System',
-		image: 'fis2.png'
+		image: fis2
 	}, {
 		caption: 'Farmer Information System',
-		image: 'fis3.png'
+		image: fis3
 	}, {
 		caption: 'Farmer Information System',
-		image: 'fis4.png'
+		image: fis4
 	}, {
 		caption: 'RisToken',
-		image: 'ristoken1.png'
+		image: ristoken1
 	}, {
 		caption: 'RisToken',
-		image: 'ristoken2.png'
+		image: ristoken2
 	}, {
 		caption: 'RisToken',
-		image: 'ristoken3.png'
+		image: ristoken3
 	}, {
 		caption: 'RisToken',
-		image: 'ristoken4.png'
+		image: ristoken4
 	}
 ]);
 
